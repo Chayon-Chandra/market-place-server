6633145b449e012955b9a2e2604aaa5f83b62d8e
chayon <chayon438@gmail.com>
2026-01-20 10:11:02 +0600

    add ID api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
@@ -34,18 +34,43 @@ async function run() {
  const database = client.db("market_place");
  const productsCollection = database.collection("products");
 
+ //All job Api
    app.get('/products', async (req, res) => {
       const cursor = productsCollection.find();
       const result = await cursor.toArray();
       res.send(result)
     })
-
+// recent Api
     app.get('/latest-products', async (req, res) => {
       const cursor = productsCollection.find().limit(6);
       const result = await cursor.toArray();
       res.send(result)
 
     })
+    // delete ID Api
+    app.delete('/products', async(req, res) => {
+      const  id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    //job details ID APi
+     app.get('/products/:id', async(req, res) =>{
+
+      const id = req.params.id;
+      const query = {_id: id};
+      const result = await productsCollection.findOne(query)
+      res.send(result)
+     })
+     // post api
+     app.post('/products', async(req, res) =>  {
+      const product = req.body;
+      const result = await productsCollection.insertOne(product);
+      res.send(result)
+     })
+
+ 
 
  await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
